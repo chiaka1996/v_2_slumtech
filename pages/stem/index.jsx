9cea9f13658c0e53517308c57be720b583a6c886
osuji chiaka <chiakajunior@yahoo.com>
2025-01-07 08:14:41 +0300

    working

--- pages/stem/index.jsx
+++ pages/stem/index.jsx
@@ -31,6 +31,16 @@ export default function About() {
     <WhoCanEnter />
     <Resources />
     <Support />
+    <div 
+     style={{backgroundImage: `url(/v_3_images/have-questions.png)`, borderImage: "fill 1 linear-gradient(#00000047, #00000033)"}}
+     className="bg-center w-full h-[387px] flex items-center justify-center"
+    >
+      <div className="font-[700] text-[4.2rem] leading-[54px] max-w-[141.4rem] text-center text-[#fff]">
+      <span className="text-[#F5B021]">Have questions or need further assistance? </span>
+      Don’t hesitate to reach out! Our support team is dedicated to 
+      helping you navigate the competition and achieve your goals.
+      </div>
+    </div>
     <StemFooter />
     </main>
   )
