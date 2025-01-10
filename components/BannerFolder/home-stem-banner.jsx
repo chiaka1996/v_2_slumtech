f32a72430a1d28d3fcd262b6ed727b9103fd10ad
osuji chiaka <chiakajunior@yahoo.com>
2025-01-10 04:06:23 +0300

    working on responsiveness

--- components/BannerFolder/home-stem-banner.jsx
+++ components/BannerFolder/home-stem-banner.jsx
@@ -17,17 +17,17 @@ const Banner = ({img }) => {
     return(
        <section 
         style={{backgroundImage: `url(/v_3_images/stem-home-banner.png)`, borderImage: "fill 1 linear-gradient(#000000B2, #00000080)"}}
-        className={`font-[400] pt-[19.5rem] pb-[8rem] text-[#fff] max-small:pt-0 justify-center min-h-[100vh] w-full bg-cover bg-center bg-[#00000099] ${css.StemHomeBanner}`}
+        className={`font-[400] pt-[19.5rem] pb-[8rem] max-small:pb-0 text-[#fff] max-small:pt-[13rem] justify-center min-h-[100vh] max-small:min-h-full w-full bg-center bg-[#00000099] ${css.StemHomeBanner}`}
        >
         <div className='container'>
         <div className='max-w-[876px] flex flex-col gap-y-[1.5rem]'>
-            <button className='w-[138px] h-[34px] rounded-[25px] bg-[#14CB8A80] text-[16px] font-[600]  leading-[24px]'>Completion</button>
-            <h1 className='font-[700] text-[48px] leading-[6.4rem]'>
+            <button className='w-[138px] h-[34px] rounded-[25px] bg-[#14CB8A80] text-[16px] max-small:text-[1.4rem] font-[600] leading-[24px]'>Completion</button>
+            <h1 className='font-[700] text-[48px] max-small:text-[3rem] leading-[6.4rem] max-small:leading-[4rem]'>
            <span>Your Innovation:</span> <br/>
             <span className='text-[#F5B021]'>Join</span> the <span className='text-[#F5B021]'>SlumTech STEM Competition</span>
             </h1>
-            <p className='max-w-[542px] text-[1.8rem] leading-[3rem]'>Unleash your creativity, solve real-world problems, and compete for a chance to change your community.</p>
-             <button className='gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px] h-[50px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] leading-[3rem]'>
+            <p className='max-w-[542px] text-[1.8rem] max-small:text-[1.6rem] leading-[3rem] max-small:leading-[2.5rem]'>Unleash your creativity, solve real-world problems, and compete for a chance to change your community.</p>
+             <button className='gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px] max-small:w-[137px] h-[50px] max-small:h-[40px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] max-small:text-[1.4rem] leading-[3rem]'>
             <span>Learn more</span>
             <div className="relative h-[11px] w-[11px]">
             <Image 
@@ -41,13 +41,13 @@ const Banner = ({img }) => {
 
         <div 
          style={{backgroundImage: `url(/v_3_images/stem-home-banner3.png)`}}
-        className='mt-[9rem] relative w-full h-[25rem] rounded-[20px] p-[7rem]'>
+        className='mt-[9rem] max-small:mt-[3rem] relative w-full min-h-[25rem] rounded-[20px] p-[7rem] max-small:p-[2rem] max-small:px-[1rem] bg-center'>
             <div>
-                <div className='flex gap-x-[3rem] mb-[3.7rem]'>
-                    <h3 className='font-[500] text-[30px] leading-[32px]'>Registration Opens</h3>
+                <div className='flex gap-[3rem] mb-[3.7rem] max-small:mb-[1.5rem]'>
+                    <h3 className='font-[500] text-[30px] max-small:text-[1.8rem] leading-[32px]'>Registration Opens</h3>
                     <button className='h-[40px] w-[137px] bg-[#14CB8A80] rounded-[25px]'>12/02/2024</button>
                 </div>
-                <button className='gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px] h-[50px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] leading-[3rem]'>
+                <button className='gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px]  max-small:w-[137px] h-[50px] max-small:h-[40px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] max-small:text-[1.4rem] leading-[3rem]'>
             <span>Join now</span>
             <div className="relative h-[11px] w-[11px]">
             <Image 
@@ -59,8 +59,8 @@ const Banner = ({img }) => {
         </button>
             </div>
 
-            <div className='absolute bottom-0 right-0'>
-            <div className='relative w-[996px] h-[498px]'>
+            <div className='absolute  max-small:relative bottom-0 right-0'>
+            <div className='relative w-[996px] max-small:w-[370px] h-[498px] max-small:h-[200px]'>
             <Image 
             src="/v_3_images/stem-home-banner2.png"
             fill
--- components/Stem/homeComponent/about.jsx
+++ components/Stem/homeComponent/about.jsx
@@ -9,25 +9,25 @@ const About = () => {
       className="w-full font-[400] leading-[3rem] text-[#000] relative"
      >
         <div className='shadow-2xl shadow-[#FFEA7B66] blur-2xl w-[616px] h-[616px] rounded-full bg-[#FFEA7B66] z-10 absolute top-[-5rem] right-[-10rem] opacity-60'></div>
-        <div className='max-w-[878px] pt-[12rem] mx-auto text-center'>    
-            <h2 className='font-[700] text-[54px] leading-[64px] text-[#004658]'>About the Competition</h2>
-            <div className='text-[18px] mb-[3rem]'>
+        <div className='max-w-[878px] pt-[12rem] max-small:pt-[5rem] mx-auto text-center max-small:px-[1rem]'>    
+            <h2 className='font-[700] text-[54px] max-small:text-[3rem] leading-[64px] max-small:leading-[3.2rem] text-[#004658]'>About the Competition</h2>
+            <div className='text-[18px] max-small:mt-[0.5rem] max-small:text-[1.4rem] max-small:leading-[2.2rem] mb-[3rem] max-small:mb-[1.5rem]'>
             The SlumTech STEM Competition is an annual event dedicated to inspiring and empowering young minds 
             from underserved communities across the United States. Our mission is to promote creativity and innovation
              in science, technology, engineering, and mathematics (STEM), providing participants with the tools and resources
             to transform their ideas into tangible solutions.
             </div>
 
-             <button className='mx-auto gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px] h-[50px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] leading-[3rem]'>
-            <span>Join now</span>
-            <div className="relative h-[11px] w-[11px]">
-            <Image 
-            src="/v_3_icons/arrow.png"
-            fill
-            alt="slumtech value"
-            />
-            </div>
-        </button>
+             <button className='mx-auto gap-x-[1.5rem] flex flex-row justify-center items-center w-[198px]  max-small:w-[137px] h-[50px] max-small:h-[40px] rounded-[25px] bg-[#F5B021] text-[700] text-[#000] font-[500] text-[18px] max-small:text-[1.4rem] leading-[3rem]'>
+              <span>Join now</span>
+              <div className="relative h-[11px] w-[11px]">
+              <Image 
+              src="/v_3_icons/arrow.png"
+              fill
+              alt="slumtech value"
+              />
+              </div>
+          </button>
         </div>
         <div 
           style={{backgroundImage: `url(/v_3_images/stem-home-about.png)`, borderImage: "fill 1 linear-gradient(#FFFFFF, #FFFFFF00)"}}
