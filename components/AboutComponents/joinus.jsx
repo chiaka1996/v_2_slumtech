34f61eebe57b684bac27162dedd6d464ca57ecc8
osuji chiaka <chiakajunior@yahoo.com>
2025-01-17 17:58:20 +0300

    working on the about page

--- components/AboutComponents/joinus.jsx
+++ components/AboutComponents/joinus.jsx
@@ -14,18 +14,21 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
 const Join = ({img }) => {
+    useEffect(() => {
+        bottomToTopAnimation(".join-us")
+    }, [])
 
     return(
        <section 
        style={{backgroundImage: `url(/v_3_images/aboutjoinus.png)`, borderImage: "fill 1 linear-gradient(#00000000, #000000BF)"}}
-        className={`font-[400] text-[#fff] text-[1.6rem] max-small:text-[1.4rem] max-small:leading-[2.4rem] mt-[12rem] min-h-[526px] w-full bg-cover bg-no-repeat flex item-center`}
+        className={`font-[400] text-[#fff] text-[1.6rem] max-small:text-[1.4rem] max-small:leading-[2.4rem] mt-[12rem] max-small:mt-0 min-h-[526px] w-full bg-cover max-small:bg-center bg-no-repeat flex item-center`}
        >
         <div className='container'>
-            <div className='max-w-[676px]'>
-            <h2 className='font-[700] text-[4.8rem] leading-[62px]'>
+            <div className='max-w-[676px] join-us'>
+            <h2 className='font-[700] text-[4.8rem] max-small:text-[3rem] leading-[62px] max-small:leading-[3.2rem]'>
                 <span className='text-[#F5B021]'>Join Us </span> <span>in Creating Impact</span>
             </h2>
-            <p className='font-[500] text-[2rem] leading-[32px] my-[2rem]'>
+            <p className='font-[500] text-[2rem] max-small:text-[1.6rem] leading-[32px] max-small:leading-[2.4rem] my-[2rem]'>
             At SlumTech Foundation, partnership is key to amplifying our impact. We welcome collaboration with local communities, 
             government agencies, NGOs, tech companies, and individuals who share our vision of empowering underserved communities.
             </p>
--- components/AboutComponents/ourprograms.jsx
+++ components/AboutComponents/ourprograms.jsx
@@ -14,33 +14,26 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(TextPlugin);
 
 const Programs = ({img }) => {
-    // useEffect(() => {
-    //     topToBottomAnimation(".programHeader")
-    //     bottomToTopAnimation(".programSububHeader")
-    //    rightToLeftAnimation(".techLiteracy")
-    //    leftToRightAnimation(".healthTech")
-    //    rightToLeftAnimation(".techEducation")
-    //    leftToRightAnimation(".entrepreneurship")
-    //    scaleUpAnimation(".programImage")
-    //    scaleUpAnimation(".globalImage")
-    //    topToBottomAnimation(".globalHeader")
-    //    bottomToTopAnimation(".countries")
+    useEffect(() => {
+        topToBottomAnimation(".programHeader")
+        bottomToTopAnimation(".program-grid")
+       
 
-    // }, [])
+    }, [])
     
 
     return(
        <section 
-        className={`font-[400] text-[1.6rem] max-small:text-[1.4rem] leading-[2.4rem] pt-[12rem] max-small:pt-[5rem] w-full`}
+        className={`font-[400] text-[1.6rem] max-small:text-[1.4rem] leading-[2.4rem] pt-[12rem] max-small:py-[5rem] w-full`}
        >
         <div className='container'>
-        <h1 className='font-[600] text-[4.8rem] leading-[62px] text-center'>
+        <h1 className='font-[600] text-[4.8rem] max-small:text-[3rem] leading-[62px] max-small:leading-[3.2rem] text-center programHeader'>
             <span className='text-[#004658]'>Our</span><span className='text-[#0C9D69]'> Programs</span>
         </h1>
 
-        <div className='mt-[5rem] max-small:mt-[3rem] gridStyle gap-[3rem] rounded-[20px]'>
+        <div className='mt-[5rem] max-small:mt-[3rem] gridStyle gap-[3rem] program-grid'>
             <div className='bg-[#F5F5F5] rounded-[20px]'>
-                <div className='relative w-full h-[30.8rem] rounded-[20px]'>
+                <div className='relative w-full h-[30.8rem] max-small:h-[20.2rem] rounded-[20px]'>
                  <Image 
                 src="/v_3_images/ourprogram1.png"
                 fill
@@ -48,9 +41,9 @@ const Programs = ({img }) => {
                 className='rounded-[20px]'
             />
                 </div>
-                <div className='py-[3rem] px-[2.5rem] rounded-[20px]'>
-                    <h2 className='text-[#004658] font-[700] text-[2.4rem] leading-[31px]'>STEM Competitions</h2>
-                    <p className='leading-[3rem] text-[1.8rem]'>
+                <div className='py-[2rem] px-[2.5rem] rounded-[20px]'>
+                    <h2 className='text-[#004658] font-[700] text-[2.4rem] max-small:text-[1.8rem] max-small:leading-[2.4rem] leading-[31px]'>STEM Competitions</h2>
+                    <p className='leading-[3rem] text-[1.8rem] max-small:text-[1.4rem] max-small:leading-[2rem]'>
                     Our STEM competitions are designed to ignite creativity and problem-solving skills among participants. 
                     These events not only promote teamwork and collaboration but also provide valuable experience and recognition 
                     for budding innovators.
@@ -60,7 +53,7 @@ const Programs = ({img }) => {
 
             {/* grid2 */}
             <div className='bg-[#F5F5F5] rounded-[20px]'>
-                <div className='relative w-full h-[30.8rem] rounded-[20px]'>
+                <div className='relative w-full h-[30.8rem] max-small:h-[20.2rem] rounded-[20px]'>
                  <Image 
                 src="/v_3_images/ourprogram2.png"
                 fill
@@ -68,9 +61,9 @@ const Programs = ({img }) => {
                 className='rounded-[20px]'
             />
                 </div>
-                <div className='py-[3rem] px-[2.5rem] rounded-b-[20px]'>
-                    <h2 className='text-[#004658] font-[700] text-[2.4rem] leading-[31px]'>Free Technology Courses and Training Platform</h2>
-                    <p className='leading-[3rem] text-[1.8rem]'>
+                <div className='py-[2rem] px-[2.5rem] rounded-b-[20px]'>
+                    <h2 className='text-[#004658] font-[700] text-[2.4rem] max-small:text-[1.8rem] max-small:leading-[2.4rem] leading-[31px]'>Free Technology Courses and Training Platform</h2>
+                    <p className='leading-[3rem] text-[1.8rem] max-small:text-[1.4rem] max-small:leading-[2rem]'>
                     We offer a comprehensive online platform featuring free technology courses. Covering a wide array of subjects—coding,
                      digital marketing, data science, and more—our training resources empower learners to gain essential skills and advance their careers.
                     </p>
@@ -78,7 +71,7 @@ const Programs = ({img }) => {
             </div>
             {/* grid3 */}
             <div className='bg-[#F5F5F5] rounded-[20px]'>
-                <div className='relative w-full h-[30.8rem] rounded-[20px]'>
+                <div className='relative w-full h-[30.8rem] max-small:h-[20.2rem] rounded-[20px]'>
                  <Image 
                 src="/v_3_images/ourprogram3.png"
                 fill
@@ -86,9 +79,9 @@ const Programs = ({img }) => {
                 className='rounded-[20px]'
             />
                 </div>
-                <div className='py-[3rem] px-[2.5rem] rounded-[20px]'>
-                    <h2 className='text-[#004658] font-[700] text-[2.4rem] leading-[31px]'>Mentorship from Tech Professionals Worldwide</h2>
-                    <p className='leading-[3rem] text-[1.8rem]'>
+                <div className='py-[2rem] px-[2.5rem] rounded-[20px]'>
+                    <h2 className='text-[#004658] font-[700] text-[2.4rem] max-small:text-[1.8rem] max-small:leading-[2.4rem] leading-[31px]'>Mentorship from Tech Professionals Worldwide</h2>
+                    <p className='leading-[3rem] text-[1.8rem] max-small:text-[1.4rem] max-small:leading-[2rem]'>
                     Our mentorship program connects aspiring tech enthusiasts with seasoned professionals 
                     from around the globe. Through one-on-one mentoring, 
                     workshops, and networking opportunities, participants 
